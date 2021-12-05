2440194953250c4be3e3c7ba7e673aeaad1cbc3f
ChronoMayne <83794920+ChronoMayne@users.noreply.github.com>
2021-12-04 19:38:08 -0600

    Added textures and gender textures.

--- src/main/java/com/github/wolfshotz/wyrmroost/client/model/entity/RoyalRedModelGecko.java
+++ src/main/java/com/github/wolfshotz/wyrmroost/client/model/entity/RoyalRedModelGecko.java
@@ -3,26 +3,40 @@ package com.github.wolfshotz.wyrmroost.client.model.entity;
 import com.github.wolfshotz.wyrmroost.Wyrmroost;
 import com.github.wolfshotz.wyrmroost.entities.dragon.RoyalRedEntity;
 import net.minecraft.util.ResourceLocation;
+import software.bernie.geckolib3.core.PlayState;
+import software.bernie.geckolib3.core.builder.AnimationBuilder;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
 
-public class RoyalRedModelGecko extends AnimatedGeoModel<RoyalRedEntity>
-{
+
+public class RoyalRedModelGecko extends AnimatedGeoModel<RoyalRedEntity> {
     @Override
-    public ResourceLocation getModelLocation(RoyalRedEntity object)
-    {
+    public ResourceLocation getModelLocation(RoyalRedEntity object) {
         return new ResourceLocation(Wyrmroost.MOD_ID, "geo/royalredgeo.json");
     }
 
     @Override
-    public ResourceLocation getTextureLocation(RoyalRedEntity object)
-    {
-        return new ResourceLocation(Wyrmroost.MOD_ID, "textures/entity/dragon/royal_red/royalred_red.png");
-    }
+    public ResourceLocation getTextureLocation(RoyalRedEntity entity) {
 
-    @Override
-    public ResourceLocation getAnimationFileLocation(RoyalRedEntity object)
-    {
-        return new ResourceLocation(Wyrmroost.MOD_ID, "animations/royalred.animation.json");
-    }
+        if (entity.getVariant() == -1 && entity.isMale()) {
+            return new ResourceLocation(Wyrmroost.MOD_ID, "textures/entity/dragon/royal_red/male_spe.png");
 
-}
+        } else if (entity.getVariant() == -1 && !entity.isMale()) {
+            return new ResourceLocation(Wyrmroost.MOD_ID, "textures/entity/dragon/royal_red/female_spe.png");
+
+        } else if (entity.getVariant() != -1 && entity.isMale()) {
+            return new ResourceLocation(Wyrmroost.MOD_ID, "textures/entity/dragon/royal_red/male.png");
+
+        } else if (entity.getVariant() != -1 && !entity.isMale()) {
+            return new ResourceLocation(Wyrmroost.MOD_ID, "textures/entity/dragon/royal_red/female.png");
+        } else
+            return new ResourceLocation(Wyrmroost.MOD_ID, "textures/entity/dragon/royal_red/female.png");
+        }
+
+
+        @Override
+        public ResourceLocation getAnimationFileLocation (RoyalRedEntity object)
+        {
+            return new ResourceLocation(Wyrmroost.MOD_ID, "animations/royalred.animation.json");
+        }
+
+    }
